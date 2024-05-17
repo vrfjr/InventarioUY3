9ee3c13f51f67505f8fe79c4320897aab3303190
Vagner da Rocha Figueiredo Junior <113543560+vrfjr@users.noreply.github.com>
2024-05-17 17:29:44 -0300

    Update Teclado.cs

--- Models/Teclado.cs
+++ Models/Teclado.cs
@@ -3,6 +3,6 @@ public class Teclado : Equipamento
     // Construtor para inicializar o Teclado com nome, número de patrimônio e outras propriedades específicas
     public Teclado(string nome, string numeroPatrimonio) : base(nome, numeroPatrimonio)
     {
-        // Você pode inicializar propriedades específicas de Teclado aqui, se necessário
+       
     }
 }
